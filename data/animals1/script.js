0a120f13f3d818d67d8d3be22a6ede232865b7b6
minzhang190 <minzhang190@gmail.com>
2020-04-20 03:22:03 +0000

    +animals1/2

---
+++ data/animals1/script.js
@@ -0,0 +1,12 @@
+document.title = "Dòngwu (1)";
+
+loadSound([
+    "5e9cda08037c6b87b73250f1.mp3",
+    "5e9cda09037c6b87b73250f2.mp3",
+    "5e9cda09037c6b87b73250f3.mp3",
+    "5e9cda09037c6b87b73250f4.mp3",
+    "5e8dab8369c2516911ea4531.mp3",
+    "5e8dab8369c2516911ea4530.mp3",
+    "5e9cda0b037c6b87b73250f5.mp3",
+    "5e9cda0b037c6b87b73250f6.mp3"
+]);
