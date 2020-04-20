4606636ea5fdd4f3e2ec010f5a73160cbfeb13d2
minzhang190 <minzhang190@gmail.com>
2020-04-20 00:40:40 +0000

    +20200419g4a/b

---
+++ data/20200419g4a/script.js
@@ -0,0 +1,17 @@
+document.title = "Jìjié Hé Xīngqī";
+
+loadSound([
+    "5e9ccac2d50a14ecf823ab95.mp3",
+    "5e9ccac3d50a14ecf823ab96.mp3",
+    "5e9ccac4d50a14ecf823ab97.mp3",
+    "5e9ccac5d50a14ecf823ab98.mp3",
+    "5e9ccac6d50a14ecf823ab99.mp3",
+    "5e9cd58217c76e7d6c69f908.mp3",
+    "5e8dad1da5b7500807899dbf.mp3",
+    "5e8dad1da5b7500807899dc0.mp3",
+    "5e8dad1da5b7500807899dc1.mp3",
+    "5e8dad1da5b7500807899dc2.mp3",
+    "5e8dad1da5b7500807899dc3.mp3",
+    "5e8dad1da5b7500807899dc4.mp3",
+    "5e8dad1da5b7500807899dc5.mp3"
+]);
---
+++ data/20200419g4b/script.js
@@ -0,0 +1,16 @@
+document.title = "Yuèfèn";
+
+loadSound([
+    "5e8dae7caf74119e4f2c03d5.mp3",
+    "5e8dae7caf74119e4f2c03d6.mp3",
+    "5e8dae7caf74119e4f2c03d7.mp3",
+    "5e8dae7caf74119e4f2c03d8.mp3",
+    "5e8dae7caf74119e4f2c03d9.mp3",
+    "5e8dae7caf74119e4f2c03da.mp3",
+    "5e8dae7caf74119e4f2c03db.mp3",
+    "5e8dae7caf74119e4f2c03dc.mp3",
+    "5e8dae7caf74119e4f2c03dd.mp3",
+    "5e8dae7caf74119e4f2c03de.mp3",
+    "5e8dae7caf74119e4f2c03df.mp3",
+    "5e8dae7caf74119e4f2c03e0.mp3"
+]);
