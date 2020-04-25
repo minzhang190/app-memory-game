8eb675432d08cb59a7841138882e907cc1a06aa9
minzhang190 <minzhang190@gmail.com>
2020-04-25 03:49:32 +0000

    +weather1/2

---
+++ data/weather1/script.js
@@ -0,0 +1,16 @@
+document.title = "Tiānqì (1)";
+
+loadSound([
+    "5e9cd3b0c9c8767483d7747a.mp3",
+    "5e9cd3b1c9c8767483d7747c.mp3",
+    "5e9cd3b3c9c8767483d7747e.mp3",
+    "5e9cd3b3c9c8767483d7747f.mp3",
+    "5e9cd3b5c9c8767483d77480.mp3",
+    "5e9cd3b6c9c8767483d77481.mp3",
+    "5e9cd3b7c9c8767483d77482.mp3",
+    "5e9cd3b9c9c8767483d77483.mp3",
+    "5e9cd3bac9c8767483d77484.mp3",
+    "5e9cd3bbc9c8767483d77485.mp3",
+    "5e9cd3bcc9c8767483d77486.mp3",
+    "5e9cd3bcc9c8767483d77487.mp3"
+]);
---
+++ data/weather2/script.js
@@ -0,0 +1,16 @@
+document.title = "Tiānqì (2)";
+
+loadSound([
+    "5e9cd3b0c9c8767483d7747b.mp3",
+    "5e9cd3b2c9c8767483d7747d.mp3",
+    "5e8db08a26af9d56f020f497.mp3",
+    "5e8db08a26af9d56f020f494.mp3",
+    "5e8db08a26af9d56f020f495.mp3",
+    "5e8db08a26af9d56f020f496.mp3",
+    "5e9cd3bdc9c8767483d77488.mp3",
+    "5e9cd3bec9c8767483d77489.mp3",
+    "5e9cd3bfc9c8767483d7748a.mp3",
+    "5e9cd3c0c9c8767483d7748b.mp3",
+    "5e9cd3c1c9c8767483d7748c.mp3",
+    "5e9cd3c2c9c8767483d7748d.mp3"
+]);
