ec707f233abce49a86daaa5da5d9ac7fa6aa6397
minzhang190 <minzhang190@gmail.com>
2020-05-18 04:01:55 +0000

    +20200517g4

---
+++ data/20200517g4/script.js
@@ -0,0 +1,15 @@
+document.title = "Jīntiān tiānqì zěnme yàng?";
+
+loadSound([
+    "5e9cd75dfb76c53278b7ea5c.mp3",
+    "5e9cd75dfb76c53278b7ea5d.mp3",
+    "5e9cd75efb76c53278b7ea5e.mp3",
+    "5e9cd75ffb76c53278b7ea5f.mp3",
+    "5e9cd75ffb76c53278b7ea60.mp3",
+    "5e9cd760fb76c53278b7ea61.mp3",
+    "5e9cd58117c76e7d6c69f905.mp3",
+    "5e9cd58217c76e7d6c69f907.mp3",
+    "5e9cd58117c76e7d6c69f906.mp3",
+    "5ea3b71497e2a3b784762d36.mp3",
+    "5ea3b71497e2a3b784762d37.mp3"
+]);
