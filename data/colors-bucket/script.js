61fbdada44b1a266565e2122ba71c8fd9d410f95
minzhang190 <minzhang190@gmail.com>
2020-10-23 07:15:57 +0000

    +colors-bucket

---
+++ data/colors-bucket/script.js
@@ -0,0 +1,16 @@
+document.title = "Yánsè";
+
+loadSound([
+    "5e8dace2ebcacede29732460.mp3",
+    "5e8dace2ebcacede2973245c.mp3",
+    "5e8dace2ebcacede29732462.mp3",
+    "5e8dace2ebcacede2973245a.mp3",
+    "5e8dace2ebcacede29732458.mp3",
+    "5e8dace2ebcacede2973245e.mp3",
+    "5e8dace2ebcacede29732457.mp3",
+    "5e8dace2ebcacede29732461.mp3",
+    "5e8dace2ebcacede29732459.mp3",
+    "5e8dace2ebcacede2973245d.mp3",
+    "5e8dace2ebcacede2973245b.mp3",
+    "5e8dace2ebcacede2973245f.mp3"
+]);
