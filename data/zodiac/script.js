d62c6df3714a66aa086f9b0cd46038b847b79e80
minzhang190 <minzhang190@gmail.com>
2020-11-09 10:54:30 +0000

    +zodiac*

---
+++ data/zodiac/script.js
@@ -0,0 +1,16 @@
+document.title = "Shí'èr Shēngxiào";
+
+loadSound([
+    "5fa8f807972bcd05b41ead2d.mp3",
+    "5fa8f807972bcd05b41ead2e.mp3",
+    "5fa8f808972bcd05b41ead2f.mp3",
+    "5fa8f808972bcd05b41ead30.mp3",
+    "5fa8f808972bcd05b41ead31.mp3",
+    "5fa8f808972bcd05b41ead32.mp3",
+    "5fa8f808972bcd05b41ead33.mp3",
+    "5fa8f808972bcd05b41ead34.mp3",
+    "5fa8f808972bcd05b41ead35.mp3",
+    "5fa8f808972bcd05b41ead36.mp3",
+    "5fa8f808972bcd05b41ead37.mp3",
+    "5fa8f809972bcd05b41ead38.mp3"
+]);
