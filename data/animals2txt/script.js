7153aa271d394a4973a86eba06d154ddbbb0b54f
minzhang190 <minzhang190@gmail.com>
2021-01-21 03:02:15 +0000

    +animals/animals*txt

---
+++ data/animals2txt/script.js
@@ -0,0 +1,12 @@
+document.title = "Dòngwu (2)";
+
+loadSound([
+    "5e9cda0b037c6b87b73250f7.mp3",
+    "5e9cda0c037c6b87b73250f8.mp3",
+    "5e8dab8369c2516911ea4532.mp3",
+    "5e9cda0d037c6b87b73250f9.mp3",
+    "5e9cda0d037c6b87b73250fa.mp3",
+    "5e9cda0d037c6b87b73250fb.mp3",
+    "5e9cda0e037c6b87b73250fc.mp3",
+    "5e9cda0e037c6b87b73250fd.mp3"
+]);
